208fad95e93bbb9333353530f0fd15fc77f0e47f
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2025-09-04 20:44:30 +0200

    10 start

---
+++ 10_setup_dbt/worksheets_snowflake/setup_warehouse_layer.sql
@@ -0,0 +1,31 @@
+use role sysadmin;
+use database job_ads;
+create schema if not exists warehouse;
+show schemas in database job_ads;
+use role securityadmin;
+
+grant role job_ads_dlt_role to role job_ads_dbt_role;
+show grants to role job_ads_dbt_role;
+use role job_ads_dbt_role;
+
+grant usage, create table, create view on schema job_ads.warehouse to role job_ads_dbt_role;
+
+grant select, insert, update, delete on all tables in schema job_ads.warehouse to role job_ads_dbt_role;
+grant select on all views in schema job_ads.warehouse to role job_ads_dbt_role;
+
+grant select, insert, update, delete on future tables in schema job_ads.warehouse to role job_ads_dbt_role;
+grant select on future views in schema job_ads.warehouse to role job_ads_dbt_role;
+
+use role job_ads_dbt_role;
+
+select * from job_ads.staging.data_fields_job_ads limit 10;
+
+show grants on schema job_ads.warehouse;
+
+use schema job_ads.warehouse;
+
+create table test (id INTEGER);
+
+show tables;
+
+drop table test;
